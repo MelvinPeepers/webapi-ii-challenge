604ff77497c91e989bc6e551569d1bf8ed0d0f88
Melvin Peepers <peepersm@gmail.com>
2019-08-10 14:06:08 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+// Grabbing the main Express Module
 const express = require("express");
 
 // db is in data so I'll need to create posts-router.js in there instead
 const BlogPostRouter = require("./data/posts-router.js");
 
+// global objects
 const server = express();
 
 // middleware
 server.use(express.json());
 server.use("/api/posts", BlogPostRouter);
 
+// root
 server.get("/", (req, res) => {
   res.send(`
     <h2>Lambda Blog Post</h2>
