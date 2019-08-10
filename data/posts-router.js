87d57f12119ba4bd1658fcefba9ed07be79b80fc
Melvin Peepers <peepersm@gmail.com>
2019-08-10 15:22:52 -0400

    added GET /api/posts/:id

--- data/posts-router.js
+++ data/posts-router.js
@@ -7,7 +7,7 @@ const Posts = require("./db");
 //import express router
 const router = express.Router();
 
-// GET /api/posts Returns an array of all the post objects contained in the database.
+// GET /api/posts - Returns an array of all the post objects contained in the database.
 router.get("/", async (req, res) => {
   try {
     const posts = await Posts.find(req.query);
@@ -23,31 +23,41 @@ router.get("/", async (req, res) => {
 });
 // tested the above with POSTMAN
 
-// GET /api/posts/:id
+// GET /api/posts/:id - Returns the post object with the specified id.
 router.get("/:id", async (req, res) => {
   try {
-    const post = await Posts.findById(req.params.id);
+    const { id } = req.params;
+    const post = await Posts.findById(id);
 
-    if (post) {
+    if (post && post.length) {
       res.status(200).json(post);
     } else {
-      res.status(404).json({ message: "Post not found." });
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
     }
   } catch (error) {
     // log error to database
     console.log(error);
     res.status(500).json({
-      message: "Error retrieving the post."
+      error: "The post information could not be retrieved."
     });
   }
 });
 
-// GET /api/posts/:id/comments
+// GET /api/posts/:id/comments - Returns an array of all the comment objects associated with the post with the specified id.
 router.get("/:id/comments", async (req, res) => {
   try {
     const { id } = req.params;
     const comment = await Posts.findCommentById(id);
-    res.status(200).json(comment);
+
+    if (comment) {
+      res.status(200).json(comment);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
   } catch (error) {
     // log error to database
     console.log(error);
